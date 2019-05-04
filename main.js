f2fb391d7762be2aeb5f1ee055f8b5b808ff251c
dasadboy <42483448+dasadboy@users.noreply.github.com>
2019-05-04 17:48:28 -0400

    Removed files

--- main.js
+++
@@ -1,8 +0,0 @@
-(() => {
-  window.addEventListener("load", displayDateTime);
-  window.addEventListener("load", setBackground);
-
-  document.querySelector("#menu").addEventListener("mouseover", peekMenu);
-  document.querySelector(".background")
-  .addEventListener("mouseover", unpeekMenu);
-})();
